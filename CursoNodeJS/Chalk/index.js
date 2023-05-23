a7af0934b26c347414552fa145d23ba0b3886b4e
Pedroanjoss <pedrodeanjos@gmail.com>
2023-05-23 16:06:13 -0300

    Att Exercicios

---
+++ CursoNodeJS/Chalk/index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk')
+
+const nota = 5
+
+if (nota >=7) {console.log(chalk.green('Parabéns! Voce está aprovado!'))
+    
+} else {console.log(chalk.black.bold('Voce foi reprovado e precisa fazer a recuperaçao'))
+    
+}
+
