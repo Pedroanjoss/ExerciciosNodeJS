5122ca34021bbfc6140dffaf8a15bc09a245a0cb
Pedroanjoss <pedrodeanjos@gmail.com>
2023-05-23 23:06:35 -0300

    Aualizacao

--- CursoNodeJS/LendoInput/index.js
+++ CursoNodeJS/LendoInput/index.js
@@ -1,5 +1,11 @@
-const readline = require('readline').createInterface(
-
-    
-)
-
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  
+  readline.question('Qual é a sua linguagem preferida? ', (language) => {
+    console.log(`A minha linguagem preferida é: ${language}`);
+    readline.close();
+  });
+  
