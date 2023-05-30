c47262028174253f84be1c6a767fcec7c3a109a9
Pedroanjoss <pedrodeanjos@gmail.com>
2023-05-29 23:41:37 -0300

    up

--- CursoNodeJS/5 EXPRESS/Middleware/index.js
+++ CursoNodeJS/5 EXPRESS/Middleware/index.js
@@ -6,21 +6,6 @@ const path = require('path')
 
 const basePath = path.join(__dirname, 'templates')
 
-const checkAuth = function (req, res, next) {
-  req.authStatus = true
-
-  if (req.authStatus) {
-    console.log('Está logado, pode continuar')
-    next()
-  } else {
-    console.log('Nao está logado, faça o login para continuar')
-    next()
-  }
-
-}
-
-app.use(checkAuth)
-
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`)
 })
